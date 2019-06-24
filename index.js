8bea1c354c40112c36ff6007744de4625e0a067e
Cassidy Trinh <Cassidy@Cassidys-Air.fios-router.home>
2019-06-23 23:28:31 -0400

    star fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var clear = $(".clear-btn");
 var stars = $("#star-container li")
 
-stars.on('mouseover', hoverStars);
+stars.on('mouseenter', hoverStars);
+stars.on('mouseleave', clearHover);
 stars.on('click', fillStars);
-clear.on('click', clearStars);
+clear.on('click', clearFill);
 
 function hoverStars() {
   var starNum = $(this).index() + 1;
@@ -14,6 +15,7 @@ function hoverStars() {
 function fillStars() {
   var starNum = $(this).index() + 1;
   stars.removeClass('filled');
+  stars.removeClass('hover');
   stars.slice(0, starNum).addClass('filled');
 }
 
@@ -39,7 +41,9 @@ function fillStars() {
 //     }
 //   }
 // }
-function clearStars() {
-  stars.removeClass('filled');
+function clearHover() {
   stars.removeClass('hover');
+}
+function clearFill() {
+  stars.removeClass('filled');
 }
