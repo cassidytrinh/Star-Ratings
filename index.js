bea265a33760c44d58233a027ff99b17b7b03128
Cassidy Trinh <Cassidy@Cassidys-Air.fios-router.home>
2019-06-23 23:04:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var clear = $(".clear-btn");
+var stars = $("#star-container li")
+
+stars.on('mouseenter', hoverStars);
+stars.on('mouseleave', clearStars);
+stars.on('click', fillStars);
+clear.on('click', clearStars);
+
+function hoverStars() {
+  var starNum = $(this).index() + 1;
+  stars.removeClass('filled');
+  stars.removeClass('hover');
+  stars.slice(0, starNum).addClass('hover');
+}
+function fillStars() {
+  var starNum = $(this).index() + 1;
+  stars.removeClass('filled');
+  stars.slice(0, starNum).addClass('filled');
+}
+
+// function hoverStars() {
+//   stars.removeClass('filled');
+//   stars.removeClass('hover');
+//   var starNum = $(this).index() + 1;
+//   for(i=1; i <=5; i++) {
+//     numID = "#" + i.toString();
+//     if(i<=starNum) {
+//       $(numID).addClass('hover');
+//     }
+//   }
+// }
+// function fillStars() {
+//   stars.removeClass('filled');
+//   stars.removeClass('hover');
+//   var starNum = $(this).index() + 1;
+//   for(i=1; i <=5; i++) {
+//     numID = "#" + i.toString();
+//     if(i<=starNum) {
+//       $(numID).addClass('filled');
+//     }
+//   }
+// }
+function clearStars() {
+  stars.removeClass('filled');
+  stars.removeClass('hover');
+}
